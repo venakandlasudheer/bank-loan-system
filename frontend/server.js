2722e05feb48f43a8c987f55d4dd36e1032057f5
venakandlasudheer <venakandlasudheerl@gmail.com>
2026-03-15 23:34:02 +0530

    Add Node.js server for frontend

---
+++ frontend/server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 80;
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript'
+};
+
+http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') filePath = './index.html';
+
+    const ext = path.extname(filePath);
+    const contentType = mimeTypes[ext] || 'text/plain';
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('Not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content);
+        }
+    });
+}).listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
